95fb06363b094d212231426c25d63ef97bf81382
Pedro Augusto <169068215+PedroAugusto30@users.noreply.github.com>
2024-07-04 11:00:55 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Escreva seu nome e sobrenome:");
+    String nome = sc.nextLine();
+    System.out.println("Quantos anos você tem:");
+    String idade = sc.nextLine();
+    System.out.println("Qual seu altura:");
+    String altura = sc.nextLine();
+    System.out.println("Qual seu sexo:");
+    System.out.println("Digite 1 para masculino e 2 para feminino:");
+    int masculino = 1;
+    int feminino = 2;
+    String sexo = sc.nextLine();
+    
+    
+    if (sexo.equals(masculino));
+    System.out.println("masculino");
+  
+    if(sexo.equals(feminino));
+    System.out.println("feminino");
+    
+      
+
+
+   
+    
+      
+  }
+
+
+}
